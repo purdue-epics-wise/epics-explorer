345aae2fc7a9a14e2fa50224a287c8c55c1e5302
Richard <r.shiazure@outlook.com>
2015-03-26 08:48:02 -0400

    Created files

---
+++ 404.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found)
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+
+get_header(); ?>
+
+
+
+	<div id="whitesplash">
+		Page not found.
+	</div>
+
+<?php get_footer(); ?>
---
+++ content.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * The default template for displaying content
+ *
+ * Used for both single and index/archive/search.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+?>
